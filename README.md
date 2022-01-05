afec9ce746279818d5f746825262dbed6ad0a829
Francisco Alves <fjunior.alves.oliveira@gmail.com>
2022-01-05 09:28:44 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# Relatórios de atividades
+
+Script para extração da tabela de atividades do Relatório de Atividades do SEI e geração de um arquivo xlsx consolidado.
+
+## Pré-requisitos
+
+Crie um ambiente virtual python e instale as dependências com
+
+```
+pip install requirements.txt
+```
+
+## Uso
+
+Salve as tabelas em html (instruções [aqui]()) na pasta `data/<servidor>/` e execute o comando
+
+```
+python main.py <servidor>
+```
+
+Um arquivo `<servidor>.xlsx` vai ser gerado na raiz do projeto.
---
+++ main.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+from pandas import read_html
+import pandas as pd
+import os
+import re
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("servidor", help="nome do servidor na pasta data/")
+args = parser.parse_args()
+
+who = args.servidor
+
+df = pd.DataFrame()
+
+for dirname, dirnames, filenames in os.walk(f'data/{who}'):
+    for filename in filenames:
+        path = os.path.join(dirname, filename)
+        lst = read_html(path, encoding='utf-8')[0]
+        tmp = pd.DataFrame(lst).dropna(how='all')
+        tmp['SERVIDOR'] = who
+        tmp['MES'] = re.search('\d{4}-\d{2}', path).group(0)
+        tmp['ATIVIDADE'] = ''
+        tmp['PROJETO'] = ''
+        tmp['ROTINA'] = ''
+        df = pd.concat([df, tmp])
+
+
+df.to_excel(f'{who}.xlsx', index=False)
