e71ee7a23d5964a90bafa18c48925d3b1f5a6fa1
Gabriel Dornas <gabrielbdornas@gmail.com>
2022-01-27 08:58:32 -0300

    Improve main.py code Reduce identation

--- main.py
+++ main.py
@@ -14,17 +14,17 @@ who = args.servidor
 df = pd.DataFrame()
 
 for dirname, dirnames, filenames in os.walk(f'data/{who}'):
-    for filename in filenames:
-        if filename != '.gitkeep':
-          path = os.path.join(dirname, filename)
-          lst = read_html(path, encoding='utf-8')[0]
-          tmp = pd.DataFrame(lst).dropna(how='all')
-          tmp['SERVIDOR'] = who
-          tmp['MES'] = re.search('\d{4}-\d{2}', path).group(0)
-          tmp['ATIVIDADE'] = ''
-          tmp['PROJETO'] = ''
-          tmp['ROTINA'] = ''
-          df = pd.concat([df, tmp])
+  for filename in filenames:
+    if filename != '.gitkeep':
+      path = os.path.join(dirname, filename)
+      lst = read_html(path, encoding='utf-8')[0]
+      tmp = pd.DataFrame(lst).dropna(how='all')
+      tmp['SERVIDOR'] = who
+      tmp['MES'] = re.search('\d{4}-\d{2}', path).group(0)
+      tmp['ATIVIDADE'] = ''
+      tmp['PROJETO'] = ''
+      tmp['ROTINA'] = ''
+      df = pd.concat([df, tmp])
 
 
 df.to_excel(f'{who}.xlsx', index=False)
